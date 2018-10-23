beb2d0c6d1ff576153d55c1c46ea8e847ccc5b77
César Cruz <cesar.cruz182@gmail.com>
2018-10-23 17:00:27 -0500

    Todo bien

--- Introduccion JS/variables.js
+++ Introduccion JS/variables.js
@@ -1,2 +1,22 @@
-/* Variables en Javascript */
-let mi_variable = 'Hola'
+/* Variables 'tradicionales' Javascript */
+var mi_variable = 'Hola';
+
+/* Variables const */
+const variable2 = 'algo'; // Nunca cambia
+
+/* Variables con let */
+let variable3 = 'ok';
+variable3 = 20;
+
+/* Template string */
+const nombre = 'Cesar';
+const trabajo = 'Desarrollador';
+
+console.log(`Nombre: ${nombre}, Trabajo: ${trabajo}`);
+
+const contenedorApp = document.querySelector('#app');
+let html = `<ul>
+    <li>Nombre: ${nombre}</li>
+    <li>Trabajo: ${trabajo} </li>
+    `;
+contenedorApp.innerHTML = html;
