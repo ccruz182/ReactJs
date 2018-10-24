7a4c0eed8cc1ff4a6cca70368b5ca46a230f1888
Cesar Cruz <cesar.cruz182@gmail.com>
2018-10-24 12:57:03 -0500

    Spread Operator

--- Introduccion JS/objetos.js
+++ Introduccion JS/objetos.js
@@ -14,6 +14,52 @@ function Tarea (nombre, urgencia) {
 	this.urgencia = urgencia;
 }
 
+Tarea.prototype.mostrarInformacion = function() {
+	return `La tarea ${this.nombre} tiene la prioridad ${this.urgencia}`;
+	// body...
+};
+
 // Creación de objeto
 const tarea1 = new Tarea('Aprender Js', 'Urgente');
-console.log('Tarea1: ', tarea1);
+console.log('Tarea1: ', tarea1.mostrarInformacion());
+
+
+/* Destructuring */
+const aprendiendoJS = {
+	version: {
+		nueva: 'ES6',
+		anterior: 'ES5'
+	},
+	frameworks: ['React', 'VueJs', 'AngularJs']
+};
+
+let {version} = aprendiendoJS;
+console.log('Versiones: ', version);
+let {nueva} = aprendiendoJS.version;
+console.log('Versión nueva: ', nueva);
+
+/* Literal Enhacement */
+const banda = 'blink-182';
+const genero = 'Pop-Rock';
+const canciones = ['The Rock Show', 'After Midnight', 'First Date'];
+
+const blink = {banda, genero, canciones};
+console.log('blink -> ', blink);
+
+/* Funciones en objetos */
+const person = {
+	name: 'Cesar',
+	age: 21,
+	sayHello () {
+		console.log('Hello!');
+	}
+};
+
+person.sayHello();
+
+/* Spread Operator */
+const lenguajes = ['JavaScript', 'PHP', 'Python'];
+const frameworks = ['ReactJS', 'Laravel', 'DJango'];
+
+const combinacion = [...lenguajes, ...frameworks];
+console.log('Combinacion: ', combinacion);
