269a21271ff6e9e82f4a9d0dd18b1d6a87c9adc5
César Cruz <cesar.cruz182@gmail.com>
2018-10-29 21:35:17 -0600

    Se obtienen datos del formulario y se crea un objeto.

--- gastosemanal/src/componentes/Formulario.js
+++ gastosemanal/src/componentes/Formulario.js
@@ -3,12 +3,39 @@ import React, { Component } from "react";
 import { Button, Form, Icon } from "semantic-ui-react";
 
 class Formulario extends Component {
+
+  nombre = React.createRef();
+  cantidad = React.createRef();
+
+  nombreListener = (event, data) => {
+    this.nombre = data.value;
+  };
+
+  cantidadListener = (event, data) => {
+    this.cantidad = data.value;
+  };
+
+  crearGasto = (event) => {
+    event.preventDefault();
+    
+    /* Se crea objeto a enviar vía props */
+    const gasto = {
+      nombre: this.nombre,
+      cantidad: this.cantidad
+    };
+
+    /* Se envía vía props */
+
+    /* Se resetea el formulario */
+    event.currentTarget.reset();
+  };
+
   render() {
     return (
-      <Form>
+      <Form onSubmit={this.crearGasto}>
         <h2>Ingresa tus gastos aquí</h2>
-        <Form.Input label="Gasto" placeholder="Ej. Transporte" />
-        <Form.Input label="Costo" placeholder="Ej. 300" />
+        <Form.Input label="Gasto" placeholder="Ej. Transporte" onChange={this.nombreListener}/>
+        <Form.Input label="Costo" placeholder="Ej. 300" onChange={this.cantidadListener}/>
         <Button animated='fade'>
           <Button.Content visible>Agregar</Button.Content>
           <Button.Content hidden>
